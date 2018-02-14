fe9650eef2cbec3ee8d4fcc8b53a93aaa8048df1
Maxime Vidori <maxime.vidori@gmail.com>
2018-02-14 11:57:27 +0100

    Remove unrelevant code

--- examples/dependency.go
+++ examples/dependency.go
@@ -11,9 +11,8 @@ func main() {
 
 	fmt.Println(antfarm.Runner{}.
 		Task("wait", tasks.Wait(5*time.Second)).
-		Task("copy", tasks.FileCopy("/home/max/games/Champions of Norrath (USA).iso", "./out")).
 		Task("world", tasks.Print("Hello World!"), "bar", "foo").
 		Task("foo", tasks.Print("Hello Foo!")).
 		Task("bar", tasks.Print("Hello Bar!"), "foo", "wait").
-		Start("copy"))
+		Start("world"))
 }
